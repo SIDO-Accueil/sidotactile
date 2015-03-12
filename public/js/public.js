5a5cc3f9efc8156d0b119fb872a9b4d5dacd69f1
Raphael Cazenave-Leveque <raphael.cazenave@gmail.com>
2015-03-12 22:18:49 +0100

    json update

--- public/js/public.js
+++ public/js/public.js
@@ -1,76 +1,172 @@
+/*eslint-env jquery, browser */
+
 $(document).ready(function(){
+    "use strict";
 
     // INITIALISATION
-
-    $(".form1").val({
-        "v1": 50,
-        "v2": 50,
-        "v3": 50,
-        "v4": 50,
-        "v5": 50,
-        "v6": 50
+    $(".form1").val(
+    {
+        "sidome": {
+            "default": true, // the sidome is a cube by default
+            "visible": true,  // visibilty
+            "badge": "<NUMERO DE BADGE>",
+            "color": {
+                "r": 255,
+                "g": 255,
+                "b": 255
+            },
+            "nodes": {
+                "node1": {
+                    "x": -1,
+                    "y": 0,
+                    "z": 0,
+                    "faces": {       // each faces on where the node is needed
+                        "face1": 0,
+                        "face2": 0,
+                        "face3": 0,
+                        "face4": 0
+                    }
+                },
+                "node2": {
+                    "x": 0,
+                    "y": 0,
+                    "z": 1,
+                    "faces": {
+                        "face1": 0,
+                        "face2": 0,
+                        "face3": 0,
+                        "face4": 0
+                    }
+                },
+                "node3": {
+                    "x": 0,
+                    "y": 1,
+                    "z": 0,
+                    "faces": {
+                        "face1": 0,
+                        "face2": 0,
+                        "face3": 0,
+                        "face4": 0
+                    }
+                },
+                "node4": {
+                    "x": 1,
+                    "y": 0,
+                    "z": 0,
+                    "faces": {
+                        "face1": 0,
+                        "face2": 0,
+                        "face3": 0,
+                        "face4": 0
+                    }
+                },
+                "node5": {
+                    "x": 0,
+                    "y": 0,
+                    "z": -1,
+                    "faces": {
+                        "face1": 0,
+                        "face2": 0,
+                        "face3": 0,
+                        "face4": 0
+                    }
+                },
+                "node6": {
+                    "x": 0,
+                    "y": -1,
+                    "z": 0,
+                    "faces": {
+                        "face1": 0,
+                        "face2": 0,
+                        "face3": 0,
+                        "face4": 0
+                    }
+                }
+            }
+        }
     });
 
-    $(".form2").val({
-        "v1": 50,
-        "v2": 50,
-        "v3": 50,
-        "v4": 50,
-        "v5": 50,
-        "v6": 50
-    });
-
-    $(".form3").val({
-        "v1": 50,
-        "v2": 50,
-        "v3": 50,
-        "v4": 50,
-        "v5": 50,
-        "v6": 50
-    });
+    //$(".form1").val({
+    //    "v1": 50,
+    //    "v2": 50,
+    //    "v3": 50,
+    //    "v4": 50,
+    //    "v5": 50,
+    //    "v6": 50
+    //});
+    //
+    //$(".form2").val({
+    //    "v1": 50,
+    //    "v2": 50,
+    //    "v3": 50,
+    //    "v4": 50,
+    //    "v5": 50,
+    //    "v6": 50
+    //});
+    //
+    //$(".form3").val({
+    //    "v1": 50,
+    //    "v2": 50,
+    //    "v3": 50,
+    //    "v4": 50,
+    //    "v5": 50,
+    //    "v6": 50
+    //});
 
 
 	// RESET THE FORM VALUES BECAUSE SOME BROWSERS USE CACHED VALUES
-	$('.questionHidden').val(50);
+	$(".questionHidden").val(50);
 
 	// SLIDER
-	$('.slider').each(function(index,ele){
+	$(".slider").each(function(index, ele){
 		$(ele).slider({
 			min: -100,
 			max: 100,
 			slide: function(event, ui) {
-				var valuePercentage = parseInt((ui.value+100)/2);
-				var sliderRel = $(ele).attr('rel') + "";
-                $(ele).parents('.form').val()["v"+sliderRel] = valuePercentage;
-				$(ele).parents('.form').find('input[name="question'+sliderRel+'"]').val(valuePercentage);
+				var valuePercentage = parseInt((ui.value + 100) / 2);
+				var sliderRel = $(ele).attr("rel") + "";
+
+                if (sliderRel === "1") {
+                    $(ele).parents(".form").val().sidome.nodes["node" + sliderRel].x = valuePercentage / -50;
+                } else if (sliderRel === "2") {
+                    $(ele).parents(".form").val().sidome.nodes["node" + sliderRel].z = valuePercentage / 50;
+                } else if (sliderRel === "3") {
+                    $(ele).parents(".form").val().sidome.nodes["node" + sliderRel].y = valuePercentage / 50;
+                } else if (sliderRel === "4") {
+                    $(ele).parents(".form").val().sidome.nodes["node" + sliderRel].x = valuePercentage / 50;
+                } else if (sliderRel === "5") {
+                    $(ele).parents(".form").val().sidome.nodes["node" + sliderRel].z = valuePercentage / -50;
+                } else if (sliderRel === "6") {
+                    $(ele).parents(".form").val().sidome.nodes["node" + sliderRel].y = valuePercentage / -50;
+                }
 			},
 			stop: function(event, ui) {
-				var valuePercentage = parseInt((ui.value+100)/2);
-				var sliderRel = $(ele).attr('rel') + "";
-				$(ele).parents('.form').find('input[name="question'+sliderRel+'"]').val(valuePercentage);
+				var valuePercentage = parseInt((ui.value + 100) / 2);
+				var sliderRel = $(ele).attr("rel") + "";
+				$(ele).parents(".form").find("input[name=\"question" + sliderRel + "\"]").val(valuePercentage);
 			}
 		});
 	});
 
 	// BUTTONS
-	$('.buttonRestart').click(function(){
-		cleanForm($(this).parents('.form'));
+	$(".buttonRestart").click(function(){
+		cleanForm($(this).parents(".form"));
 	});
-	$('.buttonStop').click(function(){
+	$(".buttonStop").click(function(){
 		// Not sure what to do here
 	});
 
 	// PROCESSING
 	var processingInstances = [];
-	$('.sidomeImage canvas').each(function(index, ele){
+	$(".sidomeImage canvas").each(function(index, ele){
 		processingInstances.push(new Processing(ele, drawPoly));
 	});
 
 
 	// FORM SUBMIT
-	$('.formSite').submit(function(evt){
+	$(".formSite").submit(function(evt){
 		evt.stopImmediatePropagation();
-		$.post($(this).attr('action'), $(this).serialize(), function(response){
+		$.post($(this).attr("action"), $(this).serialize(), function(response){
 
 		});
 		return false;
@@ -79,22 +175,25 @@ $(document).ready(function(){
 });
 
 $(window).load(function() {
+    "use strict";
 });
 
 $(window).resize(function() {
+    "use strict";
 });
 
 function cleanForm(formDiv) {
-	$(formDiv).find('.questionHidden').val(50);
-	$(formDiv).find('.slider').slider({value: 0});
+    "use strict";
+    $(formDiv).find(".questionHidden").val(50);
+	$(formDiv).find(".slider").slider({value: 0});
 }
 
 function drawPoly(processing) {
-	processing.draw = function() {
+    "use strict";
+    processing.draw = function() {
 		// Some tests using processing, I dunno how to make the 3D (as the box) work
 		// The reference is on http://processingjs.org/reference/
-		var centerX = processing.width / 2
-		var centerY = processing.height / 2;
+
 		processing.background(0);
 		processing.fill(204, 102, 0);
 		processing.stroke(255);
